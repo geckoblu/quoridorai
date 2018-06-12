56bb77f1c27c3787fd1c9c789d495f3f2776b82d
Alessio Piccoli <alepic@geckoblu.net>
2018-06-12 15:35:23 +0200

    Update local installation script

--- install-local.sh
+++ install-local.sh
@@ -1,13 +1,64 @@
-#!/bin/sh
+#!/bin/bash
 
 abspath=$(cd ${0%/*} && echo $PWD/${0##*/})
 script_path=`dirname "$abspath"`
 
-cp ${script_path}/quoridorai.desktop ${HOME}/.local/share/applications/quoridorai.desktop
+install_jar() {
 
-cp ${script_path}/resources/icons/quoridorai.png ${HOME}/.local/share/icons/quoridorai.png
+	mkdir -p ${HOME}/.local/lib/quoridorai
+	rm -fR   ${HOME}/.local/lib/quoridorai
+    mkdir -p ${HOME}/.local/lib/quoridorai
 
-mkdir -p ${HOME}/.local/lib/quoridorai
-cp ${script_path}/../quoridorai-2.1.jar ${HOME}/.local/lib/quoridorai/quoridorai-2.1.jar
+	JARNAME=`find $1 -name "quoridorai-*.jar" -printf '%f\n'`
+	cp ${script_path}/$1/${JARNAME} ${HOME}/.local/lib/quoridorai/
 
-cp ${script_path}/quoridorai ${HOME}/.local/bin/
+    mkdir -p ${HOME}/.local/bin
+    
+	cat > ${HOME}/.local/bin/quoridorai << EOF
+#!/bin/sh
+
+java -jar ${HOME}/.local/lib/quoridorai/${JARNAME}
+EOF
+
+	chmod u+x ~/.local/bin/quoridorai
+	
+	cp ${script_path}/quoridorai.desktop ${HOME}/.local/share/applications/quoridorai.desktop
+    cp ${script_path}/resources/icons/quoridorai.png ${HOME}/.local/share/icons/quoridorai.png
+
+}
+
+JARPATH=.
+JARCOUNT=`find ${JARPATH} -name "quoridorai-*.jar" | wc -l`
+
+if [ "$JARCOUNT" == "1" ]; then
+
+	install_jar ${JARPATH}
+
+else
+
+	if [ "$JARCOUNT" == "0" ]; then
+
+		JARPATH=..
+		JARCOUNT=`find ${JARPATH} -name "quoridorai-*.jar" | wc -l`
+
+		if [ "$JARCOUNT" == "1" ]; then
+
+			install_jar ${JARPATH}
+
+		elif [ "$JARCOUNT" == "0" ]; then
+	        echo "ERROR: No quoridorai jar found".
+	        return 1
+        else
+	        echo "ERROR: Too much quoridorai jar found."
+	        return 2
+        fi
+
+	else
+
+		echo "ERROR: Too much quoridorai jar found."
+		return 2
+
+	fi
+
+fi
+
